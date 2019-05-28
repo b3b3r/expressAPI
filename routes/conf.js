022fc27f2cb2b3a037c1add7ee564f28c634700c
Bertrand Seguinotte (b3b3r) <47946775+b3b3r@users.noreply.github.com>
2019-05-28 15:05:09 +0200

    Update conf.js

--- routes/conf.js
+++ routes/conf.js
@@ -1,9 +1,1 @@
-const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'jecode4bdx',
-  database: 'sport',
-});
-module.exports = connection;
